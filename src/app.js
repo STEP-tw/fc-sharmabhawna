be6f7a2ca2e1fd69275afd2b6e17242bc7dec1d4
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-01-16 20:35:07 +0530

    Created main page for flower catalog
    Introduced request handling for main page and css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
+const { readFile } = require("fs");
+
+const send = function(res, statusCode, content) {
+	res.statusCode = statusCode;
+	res.write(content);
+	res.end();
+};
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+	console.log(req.url);
+	if (req.url == "/") {
+		readFile("./index.html", (err, data) => {
+			if (err) {
+				send(res, 404, "No such file found");
+				return;
+			}
+			send(res, 200, data);
+		});
+	}
+	if (req.url == "/style.css") {
+		readFile("./style.css", (err, data) => {
+			if (err) {
+				send(res, 404, "No such file found");
+				return;
+			}
+			send(res, 200, data);
+		});
+	}
 };
 
 // Export a function that can act as a handler
