543c21347fd263b0e99f857c2f8a860f9276cb89
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-01-16 23:06:41 +0530

    Instead of giving url of the image and gif passed it through server itself

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const send = function(res, statusCode, content) {
 };
 
 const app = (req, res) => {
-	console.log(req.url);
 	if (req.url == "/") {
 		readFile("./index.html", (err, data) => {
 			if (err) {
@@ -19,6 +18,24 @@ const app = (req, res) => {
 	}
 	if (req.url == "/style.css") {
 		readFile("./style.css", (err, data) => {
+			if (err) {
+				send(res, 404, "No such file found");
+				return;
+			}
+			send(res, 200, data);
+		});
+	}
+	if (req.url == "/watering_jar.gif") {
+		readFile("./watering_jar.gif", (err, data) => {
+			if (err) {
+				send(res, 404, "No such file found");
+				return;
+			}
+			send(res, 200, data);
+		});
+	}
+	if (req.url == "/flowers.jpg") {
+		readFile("./flowers.jpg", (err, data) => {
 			if (err) {
 				send(res, 404, "No such file found");
 				return;
