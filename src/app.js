7a367b9d98eddbb2b8d5ce93f91f517100de6645
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-01-17 10:42:46 +0530

    Extracted ifs in one function

--- src/app.js
+++ src/app.js
@@ -6,26 +6,22 @@ const send = function(res, statusCode, content) {
 	res.end();
 };
 
+const getReqiuredFilePath = function(url) {
+	if (url == "/") {
+		return "./index.html";
+	}
+	return "." + url;
+};
+
 const app = (req, res) => {
 	const reader = function(err, data) {
+		if (err) {
+			send(res, 404, "No such file found");
+			return;
+		}
 		send(res, 200, data);
 	};
-
-	if (req.url == "/") {
-		readFile("./index.html", reader);
-	}
-	if (req.url == "/style.css") {
-		readFile("./style.css", reader);
-	}
-	if (req.url == "/watering_jar.gif") {
-		readFile("./watering_jar.gif", reader);
-	}
-	if (req.url == "/flowers.jpg") {
-		readFile("./flowers.jpg", reader);
-	}
-	if (req.url == "/blink.js") {
-		readFile("./src/blink.js", reader);
-	}
+	readFile(getReqiuredFilePath(req.url), reader);
 };
 
 // Export a function that can act as a handler
