807af04cedc3638b643f0aebf4ebdde4c73f4a3e
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-01-22 00:54:12 +0530

    Renamed blink.js to main.js and moved updateComments to script file

--- public/blink.js
+++
@@ -1,5 +0,0 @@
-const blink = function() {
-	const jar = document.getElementById("jar");
-	jar.style.visibility = "hidden";
-	setTimeout(() => (jar.style.visibility = "visible"), 1000);
-};
---
+++ public/main.js
@@ -0,0 +1,15 @@
+const blink = function() {
+	const jar = document.getElementById("jar");
+	jar.style.visibility = "hidden";
+	setTimeout(() => (jar.style.visibility = "visible"), 1000);
+};
+
+const updateComments = function() {
+	fetch("/comments")
+		.then(function(response) {
+			return response.text();
+		})
+		.then(function(comments) {
+			document.getElementById("comments_container").innerHTML = comments;
+		});
+};
